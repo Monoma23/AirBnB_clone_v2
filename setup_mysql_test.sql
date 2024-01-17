3aa63bbba33d6055488896b438e6976beadd2bcf
Monoma23 <wydadi2352@gmail.com>
2024-01-17 15:58:52 +0100

    ad44

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,4 +1,4 @@
---preparing a MySQL server for our project
+-- preparing a MySQL server for our project
 
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
